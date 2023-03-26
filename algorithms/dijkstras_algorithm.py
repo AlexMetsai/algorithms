1c563b5aa57ffe1cc23460f76713d30ed043940a
Alexandros Metsai <alexmetsai@gmail.com>
2023-03-26 22:21:05 +0200

    chore: dummy adjacent matrix data

--- algorithms/dijkstras_algorithm.py
+++ algorithms/dijkstras_algorithm.py
@@ -11,3 +11,20 @@ class Graph:
     def __init__(self, adjacent_matrix: List[List]):
         self.num_vertices = len(adjacent_matrix)
         self.graph = adjacent_matrix
+
+def dummy_adj_matrix():
+    """
+    A quick example I created, maybe I should look into something
+    more focused for testing.
+    """
+    adjacent_matrix = [
+        [0, 4, 5, 0, 0,],
+        [4, 0, 0, 3, 9,],
+        [5, 0, 0, 9, 0,],
+        [0, 3, 9, 0, 1,],
+        [0, 9, 0, 1, 0,]
+    ]
+
+
+if __name__ == "__main__":
+    ...
